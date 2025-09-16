09b167d774b44db3b0dbae7177c1247d94748fab
gksgustmd1 <gksgustmd1@naver.com>
2025-09-17 07:48:28 +0900

    login-button text fix

--- src/login-button.js
+++ src/login-button.js
@@ -1,6 +1,6 @@
 function LoginButton({ navigate }) {
   return (
-    <button onClick={() => navigate("/login")} style={{ margin: "10px", padding: "10px 20px" }}>로그인</button>
+    <button onClick={() => navigate("/login")} style={{ margin: "10px", padding: "10px 20px" }}>로그인/회원가입</button>
   );
 }
 
